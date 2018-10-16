f0296ffe28150f81e2bc225d1e1e796119fa83e8
Silje <39127195+staadheim@users.noreply.github.com>
2018-10-16 10:05:30 +0200

    Add files via upload

---
+++ utskrift.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char a, b, c, d;  //deklasjon
+	char e = 'o';     //deklasjon + initialisering 
+
+	a = 'H'; 
+	b = 'a'; 
+	c = 'l'; 
+	d = 108;     //Num verdi for "l" (ascii kode)
+
+	printf("%c%c%c%c%c\n", a, b, c, d, e); 
+
+	return 0; 
+}
